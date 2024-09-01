a7b84d131cf6bd20c27fbe27f17a47c8c6485d1e
ThalesGeromine <153413010+ThalesGeromine@users.noreply.github.com>
2024-09-01 18:56:52 +1000

    starting the homepage layout

--- app/src/main/java/com/example/ecommerceict372/MainActivity.java
+++ app/src/main/java/com/example/ecommerceict372/MainActivity.java
@@ -12,30 +12,13 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView listViewMenu;
-    TextView textView;
-    String[] listMenu;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listViewMenu=(ListView)findViewById(R.id.listViewMenu);
-        textView=(TextView)findViewById(R.id.textViewHead);
-        listMenu= getResources().getStringArray(R.array.arrayMenu);
-        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, listMenu);
-        listViewMenu.setAdapter(adapter);
-
-        listViewMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String value=adapter.getItem(i);
-                Toast.makeText(getApplicationContext(),value, Toast.LENGTH_SHORT).show();
-
-            }
-        });
 
 
     }
